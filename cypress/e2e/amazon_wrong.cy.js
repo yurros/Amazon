90c94d8b92a5b755c0fb534f808a6b47f901a430
Yuriy Ros <yuriyros@MacBook-Pro-Yuriy.local>
2023-11-03 13:10:24 -0400

    initial commit

--- cypress/e2e/amazon_wrong.cy.js
+++ cypress/e2e/amazon_wrong.cy.js
@@ -1,18 +1,10 @@
-describe("Sign in", () => {
-  let email;
-  let username;
-
-  it("should navigate to the sign-in page", () => {
-    // open https://cnn.com/
-    cy.visit("https://www.amazon.com/");
-
-    cy.get("#nav-link-accountList").click();
 
     // generate random integer from 1000 to 9999
     const rnd = Math.round(Math.random() * 8999) + 1000;
 
     // username was not registered before
-    const username = "alexesuar_" + rnd;
+    const username = "alexesuar_" + rnd; 
+    djhgfhjdsf
 
     // email was not registered before
     const email = username + "@gmail.com";
